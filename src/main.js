0b3e2453ab59c3fa96c113be036fe6cc314f13ef
CherryFla <843479000@qq.com>
2020-01-16 14:14:34 +0800

    add goods add and list page

--- src/main.js
+++ src/main.js
@@ -8,7 +8,14 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+//导入全局样式表
 import TreeTable from 'vue-table-with-tree-grid'
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
@@ -20,6 +27,22 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.component('tree-table', TreeTable)
+//将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
